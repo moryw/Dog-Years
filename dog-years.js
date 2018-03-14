05f3f27e58b92189cd7a434e67007f29d57daf76
moryw <matt@skitskat.xyz>
2018-03-14 12:02:22 -0400

    Add comments to program

--- dog-years.js
+++ dog-years.js
@@ -36,9 +36,17 @@ prompt(`Hello ${usersName} would you like to know how are you are in Dog Years?`
 //ask for users age
 const usersAge = prompt('How old are you?')
 
+
+//the first 2 human years equal 10.5 dog years per year
 let earlyYears = 2 * 10.5;
+
+//each human year after the first 2 are equal to 4 dogs years
 let laterYears = (usersAge - 2) * 4;
+
+//the sum of earlyYears and laterYears is the users age in dog years
 const usersAgeInDogYears = earlyYears + laterYears;
+
+
 console.log(`Thanks ${usersName}. You are ${usersAgeInDogYears} years old in dog years.`);
 
 
