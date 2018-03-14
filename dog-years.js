a1ee9ab72daca6f52da0f6f271c07deb6fd232fb
moryw <matt@skitskat.xyz>
2018-03-14 11:46:42 -0400

    Recreate program to prompt for name and age and simplify equations.

--- dog-years.js
+++ dog-years.js
@@ -17,7 +17,40 @@ const myAgeInDogYears = earlyYears + laterYears;
 
 //get users name and make it lowercase
 let myName = 'Matt'
-
-myName.toLowerCase()
+myName = myName.toLowerCase()
 
 console.log(`My name is ${myName}. I am ${myAgeInDogYears} years old in dog years.`);
+
+
+
+//this is a program that calculates a users age into dog years
+
+//ask for users name
+const usersName = prompt('What is your name?')
+
+
+//ask if user wants to know their age in dog years
+prompt(`Hello ${usersName} would you like to know how are you are in Dog Years?`)
+
+
+//ask for users age
+const usersAge = prompt('How old are you?')
+
+let earlyYears = 2 * 10.5;
+let laterYears = (usersAge - 2) * 4;
+const usersAgeInDogYears = earlyYears + laterYears;
+console.log(`Thanks ${usersName}. You are ${usersAgeInDogYears} years old in dog years.`);
+
+
+
+
+
+
+
+
+
+
+
+
+
+// end
