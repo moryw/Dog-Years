952ad8a3faf1a230204b268317614989bf302080
moryw <matt@skitskat.xyz>
2018-03-14 11:04:21 -0400

    Add variable earlyYears to be multiplied by 10.5

--- dog-years.js
+++ dog-years.js
@@ -1,2 +1,7 @@
 //age of user that will be converted into dog years
-cosnt myAge = 29
+const myAge = 29;
+
+//the first 2 human years equal 10.5 dog years per year
+let earlyYears = 2;
+
+earlyYears *= 10.5;
