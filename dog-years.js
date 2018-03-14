c71d614ad8e9bddc51db66cca819e49a1048750c
moryw <matt@skitskat.xyz>
2018-03-14 11:14:43 -0400

    Add laterYears and myAgeInDogYears variables.

--- dog-years.js
+++ dog-years.js
@@ -5,3 +5,12 @@ const myAge = 29;
 let earlyYears = 2;
 
 earlyYears *= 10.5;
+
+//take to years off of myAge to account for earlyYears
+let laterYears = myAge - 2;
+
+//each human year after the first 2 are equal to 4 dogs years
+laterYears *= 4;
+
+//the sum of earlyYears and laterYears is the users age in dog years
+const myAgeInDogYears = earlyYears + laterYears;
